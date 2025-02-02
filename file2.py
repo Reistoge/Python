8f9b347f2bb5252e78853b7f911133776bba2cb4
Reistoge <ferran.roj@gmail.com>
2025-02-02 17:38:26 -0300

    OOP thing

---
+++ file2.py
@@ -0,0 +1,11 @@
+class Animal:
+    
+    def __init__(self, name,age):
+        self.name =name
+        self.age=age
+
+class Dog(Animal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+     
